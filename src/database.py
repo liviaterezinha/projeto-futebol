6a8ec35922b687346d9e5d7ce14bc725a0949113
liviaterezinha95@gmail.com <liviaterezinha95@gmail.com>
2026-02-02 23:53:35 -0300

    Adicionar banco de dados SQLite com operações CRUD para partidas

--- src/database.py
+++ src/database.py
@@ -15,6 +15,12 @@ class Database:
             time2 TEXT NOT NULL,
             local TEXT NOT NULL,
             rodada INTEGER NOT NULL,
+            gols_jogador1 INTEGER DEFAULT 0,
+            gols_jogador2 INTEGER DEFAULT 0,
+            cartoes_amarelos_j1 INTEGER DEFAULT 0,
+            cartoes_amarelos_j2 INTEGER DEFAULT 0,
+            cartoes_vermelhos_j1 INTEGER DEFAULT 0,
+            cartoes_vermelhos_j2 INTEGER DEFAULT 0
         );
         """
         self.conn.execute(query)
@@ -23,8 +29,8 @@ class Database:
 
     def inserir_partida(self, dados):
         query = """
-        INSERT INTO partidas (jogador1, jogador2, time1, time2, local, rodada)
-        VALUES (?, ?, ?, ?, ?, ?);
+        INSERT INTO partidas (jogador1, jogador2, time1, time2, local, rodada, gols_jogador1, gols_jogador2, cartoes_amarelos_j1, cartoes_amarelos_j2, cartoes_vermelhos_j1, cartoes_vermelhos_j2)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
         """
         self.conn.execute(query, dados)
         self.conn.commit()
@@ -37,4 +43,46 @@ class Database:
     def deletar_partida(self, partida_id):
         query = "DELETE FROM partidas WHERE id = ?;"
         self.conn.execute(query, (partida_id,))
+        self.conn.commit()
+
+    def buscar_partidas(self):
+        cursor = self.conn.execute("""
+                       SELECT jogador1, jogador2, gols_jogador1, gols_jogador2,
+                       cartoes_amarelos_j1, cartoes_amarelos_j2, cartoes_vermelhos_j1, cartoes_vermelhos_j2 FROM partidas
+        """)
+        rows = cursor.fetchall()
+    
+        partidas = []
+        for r in rows:
+            partidas.append({
+                "jogador1": r[0],
+                "jogador2": r[1],
+                "gols_jogador1": r[2],
+                "gols_jogador2": r[3],
+                "cartoes_amarelos_j1": r[4],
+                "cartoes_amarelos_j2": r[5],
+                "cartoes_vermelhos_j1": r[6],
+                "cartoes_vermelhos_j2": r[7]
+            })
+
+        return partidas
+
+    def atualizar_partida(self, partida_id, dados):
+        query = """
+        UPDATE partidas SET
+        jogador1 = ?,
+        jogador2 = ?,
+        time1 = ?,
+        time2 = ?,
+        local = ?,
+        rodada = ?,
+        gols_jogador1 = ?,
+        gols_jogador2 = ?,
+        cartoes_amarelos_j1 = ?,
+        cartoes_amarelos_j2 = ?,
+        cartoes_vermelhos_j1 = ?,
+        cartoes_vermelhos_j2 = ?
+        WHERE id = ?;
+        """
+        self.conn.execute(query, (*dados, partida_id))
         self.conn.commit()
