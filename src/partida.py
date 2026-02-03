747557f51f0b7408366d886e9ad931a3e0c3d809
liviaterezinha95@gmail.com <liviaterezinha95@gmail.com>
2026-02-02 23:54:26 -0300

    Criar classe Partida para representar partidas e seus atributos

---
+++ src/partida.py
@@ -0,0 +1,11 @@
+class Partida:
+    def __init__(self, jogador1, jogador2, time1, time2, local, rodada, gols_jogador1, gols_jogador2):
+        self.jogador1 = jogador1
+        self.jogador2 = jogador2
+        self.time1 = time1
+        self.time2 = time2
+        self.local = local
+        self.rodada = rodada
+        self.gols_jogador1 = gols_jogador1
+        self.gols_jogador2 = gols_jogador2
+        
